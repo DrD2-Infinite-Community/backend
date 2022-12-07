7749099ce47fb7e5877ff4851213e499782ea197
pitossomo <pedro.h.teles@hotmail.com>
2022-12-07 17:33:14 -0300

    :truck: chore: renomeando função

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,9 @@
 import { Request, Response, Router } from "express";
-import { create , getAll} from "../controllers/user.controller";
-import {userExits} from "../middlewares/userExit.middlewares";
+import { create, getAll } from "../controllers/user.controller";
+import { userExists } from "../middlewares/userExists.middlewares";
 
 export const router = Router();
 
 router.get("/api/v1/user", getAll);
 
-router.post("/api/v1/user", userExits, create);
+router.post("/api/v1/user", userExists, create);
