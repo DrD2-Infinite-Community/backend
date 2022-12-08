dc3401db5e1d32eeb9bfae51cfaff67ac6741b73
Johnson <johnson@github.com>
2022-12-08 10:25:33 -0300

    :wrench:chore: add mongoDB em memória

--- src/database/db.connection.ts
+++ src/database/db.connection.ts
@@ -1,12 +1,21 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-const mongo = "";
+export const connectMemoryDatabase = async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose
+    .connect(mongoUri, { dbName: "testUser" })
+    .then(() => console.log(`Conectado no banco em memoria do mongoDB com sucessor.`))
+    .catch((err) => console.error(err));
+};
 
-export const connectDatabase = () => {
+export const connectCloudDatabase = () => {
   console.log("Realizado a conexão com o MongoDB Atlas...");
-
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const url = process.env.MONGO_URL!;
   mongoose
-    .connect(mongo)
+    .connect(url)
     .then(() => console.log("Conectado com MongoDB Atlas com sucessor."))
     .catch((err) => console.error(err));
 };
