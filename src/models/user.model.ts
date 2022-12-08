7aa3db61d64bfa460a721e51403f9252c3909129
Johnson <johnson@github.com>
2022-12-08 16:29:31 -0300

    :ok_hand:style: removendo os espaços em branco

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
 export interface User {
-  username: string,
-  password: string,
+  username: string;
+  password: string;
   email: string;
-};
+}
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: true,
     required: [true, "Username obrigatório"],
-    minlength: [4, "O nome do usuário deve ter no mínimo 4 caracteres"]
+    minlength: [4, "O nome do usuário deve ter no mínimo 4 caracteres"],
   },
   email: {
     type: String,
     required: true,
     lowercase: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
-    minlength:  [7, "O nome do usuário deve ter no mínimo 4 caracteres"]
+    minlength: [7, "O nome do usuário deve ter no mínimo 4 caracteres"],
   },
 });
 
