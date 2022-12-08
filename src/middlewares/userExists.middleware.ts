64c7aaf58a8ef1bbabf2c49b0b2d673623f28efd
Johnson <johnson@github.com>
2022-12-08 16:26:53 -0300

    :truck:chore: corrigindo nome do arquivo

--- src/middlewares/userExists.middleware.ts
+++ src/middlewares/userExists.middleware.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { getAUserEmail, getAUserUsername } from "../services/user.service";
-
 
 export const userExists = async (req: Request, res: Response, next: NextFunction) => {
   const { email, username } = req.body;
 
   let user = await getAUserUsername(username);
   if (user) {
-    return res.status(404).json({ mensagem: "usuário já existe." });
+    return res.status(StatusCodes.BAD_REQUEST).json({ mensagem: "usuário já existe." });
   }
 
   user = await getAUserEmail(email);
   if (user) {
-    return res.status(404).json({ mensagem: "Email já existe." });
+    return res.status(StatusCodes.BAD_REQUEST).json({ mensagem: "Email já existe." });
   }
   next();
 };
