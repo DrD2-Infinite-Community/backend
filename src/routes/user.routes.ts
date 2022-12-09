feece4c71ecf918c7f4964841e1b4bf316df3492
pitossomo <pedro.h.teles@hotmail.com>
2022-12-09 19:32:37 -0300

    :sparkles: feat(user.routes): conclui rotas crud

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create, getAll } from "../controllers/user.controller";
+import { create, getAll, getById, remove, update } from "../controllers/user.controller";
 import { userExists } from "../middlewares/userExists.middleware";
 import { requestBodyValidator } from "../middlewares/requestBodyValidator.middleware";
 
@@ -7,4 +7,10 @@ export const userRouter = Router();
 
 userRouter.get("/api/v1/user", getAll);
 
+userRouter.get("/api/v1/user/:id", userExists, getById);
+
 userRouter.post("/api/v1/user", requestBodyValidator, userExists, create);
+
+userRouter.delete("/api/v1/user/:id", userExists, remove);
+
+userRouter.put("/api/v1/user/:id", requestBodyValidator, userExists, update);
