853d44dc9bf9b2007769560423c4b05efae776e2
pitossomo <pedro.h.teles@hotmail.com>
2022-12-09 19:30:02 -0300

    :test_tube: test: add testes para crud do usuário

--- __tests__/integration/user.test.ts
+++ __tests__/integration/user.test.ts
@@ -2,6 +2,7 @@ import { testServer } from "../vitest.setup";
 import { describe, it, expect } from "vitest";
 
 describe("🧪 Rota user", () => {
+  let userId:string
   it("Deve criar um usuário", async () => {
     await testServer
       .post("/api/v1/user")
@@ -10,6 +11,7 @@ describe("🧪 Rota user", () => {
       .expect(201)
       .then((res) => {
         expect(res.body).to.have.property("username", "Mario Silvo");
+        userId = res.body.id
       });
   });
 
@@ -28,5 +30,35 @@ describe("🧪 Rota user", () => {
           ])
         );
       });
+  });
+
+  it("Deve retornar um único usuário pelo id", async () => {
+    await testServer
+      .get(`/api/v1/user/${userId}`)
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.have.property("username", "Mario Silvo");
+        expect(res.body).to.have.property("email", "mario@outlook.com");
+      });
+  });
+
+  it("Deve atualizar um usuário", async () => {
+    await testServer
+      .put(`/api/v1/user/${userId}`)
+      .send({ username: "Silvo Mauro", email: "silvo@outlook.com", password: "sal123Acucar" })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).to.have.property("username", "Silvo Mauro");
+        expect(res.body).to.have.property("email", "silvo@outlook.com");
+      });
+  });
+
+
+  it("Deleta um usuário pelo id", async () => {
+    await testServer
+      .delete(`/api/v1/user/${userId}`)
+      .expect(200)
   });
 });
