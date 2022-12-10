4d11ea70d6d9cb0de0ebaaa8771e43b1bfab4d16
Johnson <johnson@github.com>
2022-12-10 20:07:38 -0300

    :sparkles: feat(user.routes): add middleware de validação de ID

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import { create, getAll, getById, remove, update } from "../controllers/user.controller";
 import { userExists } from "../middlewares/userExists.middleware";
 import { requestBodyValidator } from "../middlewares/requestBodyValidator.middleware";
+import { isValidID } from "../middlewares/isValidID.middleware";
+import { usernameAndEmailExists } from "../middlewares/usernameAndEmailExists";
 
 export const userRouter = Router();
 
 userRouter.get("/api/v1/user", getAll);
 
-userRouter.get("/api/v1/user/:id", userExists, getById);
+userRouter.get("/api/v1/user/:id", isValidID, userExists, getById);
 
-userRouter.post("/api/v1/user", requestBodyValidator, userExists, create);
+userRouter.post("/api/v1/user", requestBodyValidator, usernameAndEmailExists, create);
 
-userRouter.delete("/api/v1/user/:id", userExists, remove);
+userRouter.delete("/api/v1/user/:id", isValidID, userExists, remove);
 
 userRouter.put("/api/v1/user/:id", requestBodyValidator, userExists, update);
