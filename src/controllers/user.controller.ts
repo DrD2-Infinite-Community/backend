d9e45621716f732b130a392d82de87f7b3650e4d
Johnson <johnson@github.com>
2022-12-10 20:02:20 -0300

    :sparkles: feat(user.controller): add verificação para a função remove

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,26 +13,37 @@ import "../translation/TranslationsYup";
 
 export const create = async (req: Request, res: Response) => {
   const userCreated = await createUser(req.body);
+
   res.status(StatusCodes.CREATED).json(userCreated);
 };
 
 export const getAll = async (req: Request, res: Response) => {
   const users = await getAllUsers();
+
   res.status(StatusCodes.OK).json(users);
 };
 
 export const getById = async (req: Request, res: Response) => {
   const user = await getUserById(req.params.id);
+
   res.status(StatusCodes.OK).json(user);
 };
 
 export const remove = async (req: Request, res: Response) => {
-  await deleteUser(req.params.id);
-  res.status(StatusCodes.OK).send();
+  const { id } = req.params;
+  const userDeleted = await deleteUser(id);
+
+  if (!userDeleted) {
+    return res.status(StatusCodes.BAD_GATEWAY).json({
+      mensagem: `Não foi possível excluir usuário de id: ${id}. Tente novamente mais tarde.`,
+    });
+  }
+
+  res.status(StatusCodes.OK).json({ mensagem: "Usuário apagado com sucessor." });
 };
 
 export const update = async (req: Request, res: Response) => {
   const userUpdated = await updateUser(req.params.id, req.body);
+
   res.status(StatusCodes.OK).json(userUpdated);
-  console.log(res);
 };
